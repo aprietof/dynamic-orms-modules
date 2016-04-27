f79dd12b7f08abc36cc74398b003cb3056a0ad31
aviflombaum <avi@flombaum.com>
2016-04-27 09:51:37 -0400

    more abstract

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,13 +7,6 @@ class Author
     :city => "TEXT",
   }
 
-  def self.attributes
-    ATTRIBUTES
-  end
-
-  ATTRIBUTES.keys.each do |attribute_name|
-    attr_accessor attribute_name
-  end
 
   include Persistable::InstanceMethods
   extend Persistable::ClassMethods
